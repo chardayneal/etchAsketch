8cf6411fa07e83289b34bba5230862c2558f631d
chardayneal <106034224+chardayneal@users.noreply.github.com>
2023-03-21 16:37:02 -0700

    Add color picker to UI, grid button js & eraser js
    added color picker to UI
    When color is selected, div's background color changes
    when clear grid button clicked, resets div background to white
    when eraser button clicked, the div background reset to white

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('grid-container');
 
-let number = 16;
+let number = 2;
 for (let i = 0; i < number; i++) {
     let gridRow = document.createElement('div');
     gridRow.setAttribute('class', 'row');
@@ -21,5 +21,75 @@ rows.forEach((row) => {
         let gridItem = document.createElement('div');
         gridItem.setAttribute('class', 'row-column')
         row.append(gridItem);
+    }
+});
+
+let colorWell;
+const defaultColor = "#0000ff";
+
+window.addEventListener('load', startup, false);
+
+function startup() {
+    colorWell = document.querySelector("#colorWell");
+    colorWell.value = defaultColor;
+    colorWell.addEventListener('input', updateFirst, false);
+    colorWell.addEventListener('change', updateAll, false);
+    colorWell.select();
+}
+
+function updateFirst(event) {
+    const chooseColor = document.querySelector('#chooseColor');
+    if (chooseColor) {
+        chooseColor.style.backgroundColor = event.target.value;
+    }
+}
+
+const gridItems = document.querySelectorAll('.row-column');
+function updateAll(event) {
+    gridItems.forEach((box) => {
+        box.addEventListener('click', () => {
+            box.style.backgroundColor = event.target.value;
+        });
+        //
+    });
+}
+
+gridItems.forEach((box) => {
+    box.addEventListener('click', () => {
+        box.style.backgroundColor = defaultColor;
+    });
+});
+
+//returns grid item to white background color
+const erase = document.getElementById('erase');
+const eraseText = document.getElementById('erase-text');
+erase.addEventListener("click", () => {
+    if (eraseText.textContent == 'Eraser Off') {
+        eraseText.textContent = 'Eraser On';
+        erase.appendChild(eraseText);
+        gridItems.forEach((item) => {
+            item.addEventListener("click", () => {
+                item.style.backgroundColor = '#ffffff';
+            });
+        });
+        
+    } else {
+        gridItems.forEach((item) => {
+            item.addEventListener("click", () => {
+                item.style.backgroundColor = colorWell.value;
+            });
+        });
+        eraseText.textContent = 'Eraser Off';
+        erase.appendChild(eraseText);
+    }
+})
+
+//add EventListener to Clear Grid button
+const clear = document.querySelector('#clearAll');
+clear.addEventListener("click", () => {
+    if(confirm('Are you sure you would like to clear grid?')) {
+        gridItems.forEach((box) => {
+            box.style.backgroundColor = '#ffffff';
+        })
     }
 });
