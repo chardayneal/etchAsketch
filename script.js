dfbe2d5b2ccb7c5ac6d4fc15c16113f392ea1383
chardayneal <106034224+chardayneal@users.noreply.github.com>
2023-03-22 16:28:04 -0700

    Modify html div / complete rainbow & erase cbs
    Added row-reverse to #sketcharea flex direction
    Added functional code for eraser checkbox
    added functional code for rainbow color checkbox
    
    Bug: we can't select different color with color picker

--- script.js
+++ script.js
@@ -1,52 +1,44 @@
 const container = document.getElementById('grid-container');
 
-let number = 2;
+// creates grid size
+let number = 16;
 for (let i = 0; i < number; i++) {
-    let gridRow = document.createElement('div');
-    gridRow.setAttribute('class', 'row');
-    (container.appendChild(gridRow)* 6);
+    let row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    container.appendChild(row);
     
 }
+const rows = document.querySelectorAll('.row');
+rows.forEach((row) => {
+    for (let i = 0; i < number; i++) {
+        let square = document.createElement('div');
+        square.setAttribute('class', 'square')
+        row.append(square);
+    }
+});
 
-//display grid size
+//display grid size on slider
 const input = document.querySelector('input');
 input.setAttribute('value', `${number}`);
 const sliderText = document.getElementById('sliderText');
 sliderText.textContent = `${number} x ${number}`;
 
 
-const rows = document.querySelectorAll('.row');
-rows.forEach((row) => {
-    for (let i = 0; i < number; i++) {
-        let gridItem = document.createElement('div');
-        gridItem.setAttribute('class', 'row-column')
-        row.append(gridItem);
-    }
-});
 
 let colorWell;
-const defaultColor = "#0000ff";
-
+const defaultColor = "#000000";
 window.addEventListener('load', startup, false);
 
 function startup() {
     colorWell = document.querySelector("#colorWell");
     colorWell.value = defaultColor;
-    colorWell.addEventListener('input', updateFirst, false);
     colorWell.addEventListener('change', updateAll, false);
     colorWell.select();
 }
 
-function updateFirst(event) {
-    const chooseColor = document.querySelector('#chooseColor');
-    if (chooseColor) {
-        chooseColor.style.backgroundColor = event.target.value;
-    }
-}
-
-const gridItems = document.querySelectorAll('.row-column');
+const squares = document.querySelectorAll('.square');
 function updateAll(event) {
-    gridItems.forEach((box) => {
+    squares.forEach((box) => {
         box.addEventListener('click', () => {
             box.style.backgroundColor = event.target.value;
         });
@@ -54,42 +46,76 @@ function updateAll(event) {
     });
 }
 
-gridItems.forEach((box) => {
+squares.forEach((box) => {
     box.addEventListener('click', () => {
         box.style.backgroundColor = defaultColor;
     });
 });
 
 //returns grid item to white background color
-const erase = document.getElementById('erase');
-const eraseText = document.getElementById('erase-text');
-erase.addEventListener("click", () => {
-    if (eraseText.textContent == 'Eraser Off') {
-        eraseText.textContent = 'Eraser On';
-        erase.appendChild(eraseText);
-        gridItems.forEach((item) => {
-            item.addEventListener("click", () => {
-                item.style.backgroundColor = '#ffffff';
-            });
-        });
-        
-    } else {
-        gridItems.forEach((item) => {
-            item.addEventListener("click", () => {
-                item.style.backgroundColor = colorWell.value;
-            });
-        });
-        eraseText.textContent = 'Eraser Off';
-        erase.appendChild(eraseText);
+const eraser = document.getElementById('eraser');
+eraser.addEventListener('click', () => {
+    if (eraser.checked == true) {
+        erase();
+    } else if (rainbowColor.checked == true) {
+        getRainbowColor();
+    }else {
+        getChosenColor();
     }
-})
+});
+
+function erase() {
+    squares.forEach((box) => {
+        box.addEventListener("click", () => {
+            box.style.backgroundColor = '#ffffff';
+        });
+    })
+}
+
+function getChosenColor() {
+    squares.forEach((box) => {
+        box.addEventListener("click", () => {
+            box.style.backgroundColor = colorWell.value;
+        });
+    });
+}
 
 //add EventListener to Clear Grid button
 const clear = document.querySelector('#clearAll');
 clear.addEventListener("click", () => {
     if(confirm('Are you sure you would like to clear grid?')) {
-        gridItems.forEach((box) => {
+        squares.forEach((box) => {
             box.style.backgroundColor = '#ffffff';
         })
     }
 });
+
+//rainbow color selector
+const array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+function getRandomColor() {
+    let randomColor = '#';
+    for (let i = 0; i < 6; i++) {
+        let num = Math.floor(Math.random()* array.length);
+        randomColor += array[num];
+    }
+    return randomColor;
+}
+
+const rainbowColor = document.getElementById('rainbowColor');
+rainbowColor.addEventListener("click", () => {
+    if (rainbowColor.checked == true) {
+        getRainbowColor();
+    } else if (eraser.checked == true) {
+        erase();
+    }else {
+        getChosenColor();
+    }
+});
+
+function getRainbowColor() {
+    squares.forEach((box) => {
+        box.addEventListener('click', () => {
+            box.style.backgroundColor = getRandomColor();
+        });
+    });
+}
